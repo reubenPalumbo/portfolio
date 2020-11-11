066708774d0ed4e374f6f745e7033acc354817c2
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-11-11 13:30:15 -0800

    links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
-import Footer from "./Compoents/MainComp/Footer";
-import Header from "./Compoents/MainComp/Header";
-import Body from "./Compoents/MainComp/Body";
+import Footer from "./Componets/Footer";
+import Header from "./Componets/Header";
+import Body from "./Componets/Body";
+import Links from "./Componets/Link";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Links />
       <Footer />
     </div>
   );
---
+++ src/Componets/Link.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Links() {
+  return (
+    <div className="links body">
+      <h2>Find me on:</h2>
+      <div className="linkBox">
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+        <div className="linkItem">SAMPLE TEXT</div>
+      </div>
+    </div>
+  );
+}
+
+export default Links;
