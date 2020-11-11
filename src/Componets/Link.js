d71d9009e64e3049148587616bf806517d3e5619
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-11-11 13:59:01 -0800

    Needs imgs

--- src/Componets/Link.js
+++ src/Componets/Link.js
@@ -3,17 +3,57 @@ import React from "react";
 function Links() {
   return (
     <div className="links body">
-      <h2>Find me on:</h2>
+      <h2>FIND ME ON:</h2>
       <div className="linkBox">
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
-        <div className="linkItem">SAMPLE TEXT</div>
+        <div
+          className="linkItem instagram"
+          onClick={() => {
+            window.location = "https://www.instagram.com/reubensandwichthe1st";
+          }}
+        >
+          Instagram
+        </div>
+        <div
+          className="linkItem linkdin"
+          onClick={() => {
+            window.location = "https://www.linkedin.com/in/reuben-palumbo/";
+          }}
+        >
+          Linkdin
+        </div>
+        <div
+          className="linkItem soundcloud"
+          onClick={() => {
+            window.location = "https://soundcloud.com/reuben-palumbo";
+          }}
+        >
+          Soundcloud
+        </div>
+        <div
+          className="linkItem twitter"
+          onClick={() => {
+            window.location = "https://twitter.com/palumboreuben";
+          }}
+        >
+          Twitter
+        </div>
+        <div
+          className="linkItem youtube"
+          onClick={() => {
+            window.location =
+              "https://www.youtube.com/channel/UCgbrQeXkNC9ide1EyYd9mzA";
+          }}
+        >
+          Youtube
+        </div>
+        <div
+          className="linkItem github"
+          onClick={() => {
+            window.location = "https://github.com/reubenPalumbo";
+          }}
+        >
+          GitHub
+        </div>
       </div>
     </div>
   );
