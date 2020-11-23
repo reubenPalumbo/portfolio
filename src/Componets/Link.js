42b2691b5eb8dfb8292b7d36fa833fd55c3a1378
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-11-23 14:18:09 -0800

    Added logos

--- src/Componets/Link.js
+++ src/Componets/Link.js
@@ -11,7 +11,11 @@ function Links() {
             window.location = "https://www.instagram.com/reubensandwichthe1st";
           }}
         >
-          Instagram
+          <img
+            className="imgIcon"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/58/Instagram-Icon.png"
+            alt="instgram"
+          />
         </div>
         <div
           className="linkItem linkdin"
@@ -19,7 +23,11 @@ function Links() {
             window.location = "https://www.linkedin.com/in/reuben-palumbo/";
           }}
         >
-          Linkdin
+          <img
+            className="imgIcon"
+            src="https://image.flaticon.com/icons/png/512/61/61109.png"
+            alt="linkdin"
+          />
         </div>
         <div
           className="linkItem soundcloud"
@@ -27,15 +35,24 @@ function Links() {
             window.location = "https://soundcloud.com/reuben-palumbo";
           }}
         >
-          Soundcloud
+          <img
+            className="imgIcon"
+            src="https://i1.wp.com/www.playwrightslocal.org/wp-content/uploads/2018/04/icon-soundcloud.png?fit=1024%2C1024"
+            alt="soundcloud"
+          />
         </div>
         <div
           className="linkItem twitter"
           onClick={() => {
-            window.location = "https://twitter.com/palumboreuben";
+            window.location =
+              "https://image.flaticon.com/icons/png/512/124/124021.png";
           }}
         >
-          Twitter
+          <img
+            className="imgIcon"
+            src="https://icons-for-free.com/iconfiles/png/512/social+tweet+twitter+icon-1320192248146512175.png"
+            alt="twitter"
+          />
         </div>
         <div
           className="linkItem youtube"
@@ -44,7 +61,11 @@ function Links() {
               "https://www.youtube.com/channel/UCgbrQeXkNC9ide1EyYd9mzA";
           }}
         >
-          Youtube
+          <img
+            className="imgIcon"
+            src="https://icons-for-free.com/iconfiles/png/512/video+youtube+icon-1320192294490006733.png"
+            alt="youtube"
+          />
         </div>
         <div
           className="linkItem github"
@@ -52,7 +73,11 @@ function Links() {
             window.location = "https://github.com/reubenPalumbo";
           }}
         >
-          GitHub
+          <img
+            className="imgIcon"
+            src="https://www.flaticon.com/svg/static/icons/svg/25/25231.svg"
+            alt="github"
+          />
         </div>
       </div>
     </div>
